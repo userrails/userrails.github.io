553bce7f01d1e07e32a1b81c270399f632529aa6
Shiv Raj Badu <shivrajbadu@gmail.com>
2024-02-18 17:14:04 +0545

    transitions and animations

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1708238616';
+const cacheName = 'chirpy-1708255573';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
